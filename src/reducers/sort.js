c9e1472b4eb45cfc85ec50f08bace00a859e3087
khucvandoan <doankhuc123@gmail.com>
2021-11-25 10:37:20 +0700

    quan ly cong viec

---
+++ src/reducers/sort.js
@@ -0,0 +1,18 @@
+var initialState = {
+  by: "status",
+  value: 1,
+};
+var myReducer = (state = initialState, action) => {
+  if (action.type === "SORT") {
+    var { by, value } = action.sort;
+    return {
+      sort: {
+        by,
+        value,
+      },
+    };
+  }
+
+  return state;
+};
+export default myReducer;
